84c4f38f73de7d84f81a86015e15308510c3d1e1
niamtokik <mk@steepath.eu>
2017-02-14 15:47:38 +0100

    add some manufacturer on product for testing.

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@
 PRAGMA foreign_keys = ON;
 
 -- clean table before add new content
-DROP TABLE IF EXISTS manufacturer_location
+DROP TABLE IF EXISTS manufacturer_location;
 DROP TABLE IF EXISTS manufacturer;
 DROP TABLE IF EXISTS reseller_location;
 DROP TABLE IF EXISTS reseller;
@@ -244,200 +244,228 @@ CREATE TABLE IF NOT EXISTS product (
 	  id INTEGER PRIMARY KEY
 	, ref TEXT
 	, link TEXT
+	, manufacturer INTEGER
+	, FOREIGN KEY (manufacturer) REFERENCES manufacturer(id)
 );
 
-INSERT INTO product (ref, link)
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix1e"
-       	      , "https://pcengines.ch/alix1e.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix1e.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix2d2"
-       	      , "https://pcengines.ch/alix2d2.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix2d2.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );	      
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix2d3"
-       	      , "https://pcengines.ch/alix2d3.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix2d3.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix2d13"
-       	      , "https://pcengines.ch/alix2d13.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix2d13.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix3d2"
-       	      , "https://pcengines.ch/alix3d2.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix3d2.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix3d3"
-       	      , "https://pcengines.ch/alix3d3.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix3d3.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "alix6f2"
-       	      , "https://pcengines.ch/alix6f2.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/alix6f2.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "apu"
-       	      , "https://pcengines.ch/apu.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/apu.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "apu2c0"
-       	      , "https://pcengines.ch/apu2c0.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/apu2c0.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "apu2c2"
-       	      , "https://pcengines.ch/apu2c2.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/apu2c2.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "apu2c4"
-       	      , "https://pcengines.ch/apu2c4.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/apu2c4.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "apu3a2"
-       	      , "https://pcengines.ch/apu3a2.htm");
-INSERT INTO product (ref, link)
+       	      , "https://pcengines.ch/apu3a2.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
+INSERT INTO product (ref, link, manufacturer)
        VALUES ( "apu3a4"
-       	      , "https://pcengines.ch/apu3a4.htm");
+       	      , "https://pcengines.ch/apu3a4.htm"
+	      , ((SELECT id FROM manufacturer WHERE name="pcengine"))
+	      );
 INSERT INTO product (ref, link)
        VALUES ( "net5501-60"
-       	      , "");
+       	      , "https://soekris.com/products/eol-products/net5501.html");
 INSERT INTO product (ref, link)
        VALUES ( "net5501-70"
-       	      , "");
+       	      , "https://soekris.com/products/eol-products/net5501.html");
 INSERT INTO product (ref, link)
        VALUES ( "net6501-30"
-       	      , "");
+       	      , "https://soekris.com/products/net6501-1.html");
 INSERT INTO product (ref, link)
        VALUES ( "net6501-50"
-       	      , "");
+       	      , "https://soekris.com/products/net6501-1.html");
 INSERT INTO product (ref, link)
        VALUES ( "net6501-70"
-       	      , "");
+       	      , "https://soekris.com/products/net6501-1.html");
 INSERT INTO product (ref, link)
        VALUES ( "turris-omnia"
-       	      , "");
+       	      , "https://project.turris.cz/en/");
 INSERT INTO product (ref, link)
        VALUES ( "SYS-5018A-MHN4"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/system/1U/5018/SYS-5018A-MHN4.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "SYS-5018A-MLHN4"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/system/1U/5018/SYS-5018A-MLHN4.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "SYS-5018A-MHN4"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/system/1U/5018/SYS-5018A-MLTN4.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "SYS-5018A-LTN4"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/system/1U/5018/SYS-5018A-LTN4.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "SYS-5018A-TN4"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/system/1U/5018/SYS-5018A-TN4.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "NF592-Q170"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/NF592.html");
 INSERT INTO product (ref, link)
        VALUES ( "JBC153F592"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/JBC153F592.html");
 INSERT INTO product (ref, link)
        VALUES ( "JBC153F9HB"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/JBC153F9HB.html");
 INSERT INTO product (ref, link)
        VALUES ( "JBC153F9HG"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/JBC153F9HG.html");
 INSERT INTO product (ref, link)
        VALUES ( "JBC150F592"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/JBC150F592.html");
 INSERT INTO product (ref, link)
        VALUES ( "JBC150F59H"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/JBC150F9H.html");
 INSERT INTO product (ref, link)
        VALUES ( "JBC152F533"
-       	      , "");
+       	      , "http://www.jetwaycomputer.com/JBC152F533.html");
 INSERT INTO product (ref, link)
        VALUES ( "LE-575"
-       	      , "");
+       	      , "http://www.commell.com.tw/Product/SBC/LE-575.HTM");
 INSERT INTO product (ref, link)
        VALUES ( "LS-574"
-       	      , "");
+       	      , "http://www.commell.com.tw/Product/SBC/LS-574.HTM");
 INSERT INTO product (ref, link)
        VALUES ( "LS-573"
-       	      , "");
+       	      , "http://www.commell.com.tw/Product/SBC/LS-573.HTM");
 INSERT INTO product (ref, link)
        VALUES ( "LE-564"
-       	      , "");
+       	      , "http://www.commell.com.tw/Product/SBC/LE-564.HTM");
 INSERT INTO product (ref, link)
        VALUES ( "GA-9SISL"
-       	      , "");
+       	      , "http://b2b.gigabyte.com/products/product-page.aspx?pid=4988");
 INSERT INTO product (ref, link)
        VALUES ( "EL-30"
-       	      , "");
+       	      , "http://b2b.gigabyte.com/products/product-page.aspx?pid=6192");
 INSERT INTO product (ref, link)
        VALUES ( "MS-99A1"
-       	      , "");
+       	      , "http://ipc.msi.com/product/pages/ipc/MS-99A1.html");
 INSERT INTO product (ref, link)
        VALUES ( "MS-98H1"
-       	      , "");
+       	      , "http://ipc.msi.com/product/pages/ipc/MS-98H1.html");
 INSERT INTO product (ref, link)
        VALUES ( "K-Q77NS"
-       	      , "");
+       	      , "http://www.cstipc.com/en/products/product_detial_26.html");
 INSERT INTO product (ref, link)
        VALUES ( "K-D56NS"
-       	      , "");
+       	      , "http://www.cstipc.com/en/products/product_detial_15.html");
 INSERT INTO product (ref, link)
        VALUES ( "K-945GSN"
-       	      , "");
+       	      , "http://www.cstipc.com/en/products/product_detial_16.html");
 INSERT INTO product (ref, link)
        VALUES ( "BIS-6370"
-       	      , "");
+       	      , "http://www.norco-group.com/product/39.html");
 INSERT INTO product (ref, link)
        VALUES ( "banana-pi-r1"
-       	      , "");
+       	      , "http://www.banana-pi.org/r1.html");
 INSERT INTO product (ref, link)
        VALUES ( "RTN16"
-       	      , "");
+       	      , "https://www.asus.com/Networking/RTN16");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M5F"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/114");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M5V"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/ITXM5V");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M5V_B"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/104");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M2F"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/115");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M8F"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/118");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M9F"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/ITXM9F");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-M9F_B"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/119");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-D25SL"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/116");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-G41XE"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/122");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-H67SL"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/H67SL");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-B75SL"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/MotherboardB75SL");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-H87SL_B"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/MotherboardH87SL");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-N70SL"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/N70SL");
 INSERT INTO product (ref, link)
        VALUES ( "ITX-N70SL_B"
-       	      , "");
+       	      , "http://www.inctel.com.cn/product/detail/N70SLB");
 INSERT INTO product (ref, link)
        VALUES ( "A1SRM-LN5F-2358"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/motherboard/Atom/X10/A1SRM-LN5F-2358.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "A1SRM-LN7F-2358"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/motherboard/Atom/X10/A1SRM-LN7F-2358.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "A1SRM-LN7F-2758"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/motherboard/Atom/X10/A1SRM-LN7F-2758.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "X11SSH-LN4F"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/motherboard/Xeon/C236_C232/X11SSH-LN4F.cfm");
 INSERT INTO product (ref, link)
        VALUES ( "X11SBA-LN4F"
-       	      , "");
+       	      , "https://www.supermicro.nl/products/motherboard/X11/X11SBA-LN4F.cfm");
 
 ----------------------------------------------------------------------
 -- arch table will contain specific
